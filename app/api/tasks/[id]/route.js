93eeba4b689fd1c60af6336adb3c30fc292e4c0f
MOHD AYAN AKHTAR <mohdayanakhtar@MOHDs-MacBook-Air.local>
2026-05-10 16:22:36 +0530

    fixed project delete api

--- app/api/tasks/[id]/route.js
+++ app/api/tasks/[id]/route.js
@@ -1,37 +1,6 @@
 import { connectDB } from '@/lib/db'
-import Task from '@/models/Task'
+import Project from '@/models/Project'
 import { NextResponse } from 'next/server'
-
-export async function PATCH(req, context) {
-  try {
-    await connectDB()
-
-    const { id } = await context.params
-
-    const body = await req.json()
-
-    const updatedTask = await Task.findByIdAndUpdate(
-      id,
-      {
-        status: body.status,
-      },
-      {
-        new: true,
-      }
-    )
-
-    return NextResponse.json(updatedTask)
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: error.message,
-      },
-      {
-        status: 500,
-      }
-    )
-  }
-}
 
 export async function DELETE(req, context) {
   try {
@@ -39,10 +8,10 @@ export async function DELETE(req, context) {
 
     const { id } = await context.params
 
-    await Task.findByIdAndDelete(id)
+    await Project.findByIdAndDelete(id)
 
     return NextResponse.json({
-      message: 'Task deleted successfully',
+      message: 'Project deleted successfully',
     })
   } catch (error) {
     return NextResponse.json(
